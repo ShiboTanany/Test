ff1e11ffa7527ceddc5c89e29f17a13c16030736
shibo <shibo@shibo-LIFEBOOK-AH531-GFO>
2015-12-07 15:42:27 +0200

    shibo enterd a new things

---
+++ src/main/java/com/mycompany/hellopspring/NewClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hellopspring;
+
+import java.util.Date;
+
+
+
+/**
+ *
+ * @author shibo
+ */
+public class NewClass {
+    
+    public static void main(String[] args) {
+        new Date();
+    }
+}
